9eda9640a17f47f43158370c90da248cd2c71d94
Richter96 <castiglione96.rc@gmail.com>
2023-03-14 18:56:26 +0100

    edit buttom

--- asset/js/main.js
+++ asset/js/main.js
@@ -16,10 +16,11 @@ for (i = 0; i < immagini.length; i++) {
     // collego il link immagini con una costante
     const imgSrc = immagini[i]
     // add imgSrc in a string HTml
-    const elementImg = `<img id="immagine${i}" class="img-fluid ${ i == activeimage ? "active" : ""}" src="${imgSrc}" alt="">`
+    const elementImg = `<img id="immagine${i}" class="img-fluid ${i == activeimage ? "active" : ""}" src="${imgSrc}" alt="">`
     console.log(elementImg);
     imgCard.insertAdjacentHTML('beforeend', elementImg)
 
+   
     // il for active
     // if (i === activeimage) {
     //     console.log('active')
@@ -32,18 +33,20 @@ for (i = 0; i < immagini.length; i++) {
 const slideElement = document.querySelectorAll('.card-img > img')
 
 // add funtion at clic next
-document.querySelector('.next').addEventListener('click', function(){
-    console.log('cliccato')
-    // selezionare la slaide attiva 
-    let currentSlide = slideElement[activeimage]
-    currentSlide.classList.remove('active')
-    activeimage++
-    currentSlide = slideElement[activeimage]
-    currentSlide.classList.add('active')
-    
+document.querySelector('.next').addEventListener('click', function () {
+        console.log('cliccato')
+        // selezionare la slaide attiva 
+        let currentSlide = slideElement[activeimage]
+        currentSlide.classList.remove('active')
+        activeimage++
+        if (activeimage > immagini.length) {
+            
+        }
+        currentSlide = slideElement[activeimage]
+        currentSlide.classList.add('active')
 })
 
-document.querySelector('.prev').addEventListener('click', function(){
+document.querySelector('.prev').addEventListener('click', function () {
     console.log('cliccato')
     // selezionare la slaide attiva 
     let currentSlide = slideElement[activeimage]
@@ -51,6 +54,5 @@ document.querySelector('.prev').addEventListener('click', function(){
     activeimage--
     currentSlide = slideElement[activeimage]
     currentSlide.classList.add('active')
-    
-})
 
+})
