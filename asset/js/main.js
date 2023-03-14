72f270e6456c8ec4f47907fb5abd7553fd7d90b5
Richter96 <castiglione96.rc@gmail.com>
2023-03-14 16:02:32 +0100

    add img

---
+++ asset/js/main.js
@@ -0,0 +1,9 @@
+
+const img = [
+    '<img class="img-fluid active" src="./asset/img/01.webp" alt="">',
+    '<img class="img-fluid" src="./asset/img/02.webp" alt="">',
+    '<img class="img-fluid" src="./asset/img/03.webp" alt="">',
+    '<img class="img-fluid" src="./asset/img/04.webp" alt="">',
+    '<img class="img-fluid" src="./asset/img/05.webp" alt="">'
+]
+
