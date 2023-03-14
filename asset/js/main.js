4ddca238a41e4c7183837de7108e30aaa79524e0
Richter96 <castiglione96.rc@gmail.com>
2023-03-14 16:36:56 +0100

    add img in div card

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,9 +1,19 @@
 
-const img = [
-    '<img class="img-fluid active" src="./asset/img/01.webp" alt="">',
-    '<img class="img-fluid" src="./asset/img/02.webp" alt="">',
-    '<img class="img-fluid" src="./asset/img/03.webp" alt="">',
-    '<img class="img-fluid" src="./asset/img/04.webp" alt="">',
-    '<img class="img-fluid" src="./asset/img/05.webp" alt="">'
+const immagini = [
+    './asset/img/01.webp',
+    './asset/img/02.webp',
+    './asset/img/03.webp',
+    './asset/img/04.webp',
+    './asset/img/05.webp'
 ]
 
+
+const imgCard = document.querySelector('.card-img')
+let activeimage = 0
+
+for (i = 0; i < immagini.length; i++) {
+    const img = immagini[i]
+    const elementImg = `<img class="img-fluid active" src="${img}" alt="">`
+    console.log(elementImg);
+    imgCard.insertAdjacentHTML('beforeend', elementImg)
+}
