3324e5f48dc7a7a2e4e81ab484e631b3e7e913c7
Richter96 <castiglione96.rc@gmail.com>
2023-03-14 17:01:03 +0100

    add activ with if

--- asset/js/main.js
+++ asset/js/main.js
@@ -12,8 +12,21 @@ const imgCard = document.querySelector('.card-img')
 let activeimage = 0
 
 for (i = 0; i < immagini.length; i++) {
-    const img = immagini[i]
-    const elementImg = `<img class="img-fluid active" src="${img}" alt="">`
+    console.log(i)
+    // collego il link immagini con una costante
+    const imgSrc = immagini[i]
+    // add imgSrc in a string HTml
+    const elementImg = `<img id="immagine${i}" class="img-fluid ${ i == activeimage ? "active" : ""}" src="${imgSrc}" alt="">`
     console.log(elementImg);
     imgCard.insertAdjacentHTML('beforeend', elementImg)
+
+    // il for active
+    // if (i === activeimage) {
+    //     console.log('active')
+    // } else {
+    //     console.log('non active')
+    // }
 }
+
+
+
